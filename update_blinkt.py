ce29cbc8c2c49ab78a025363f1526527816251ea
James Herbert <james@artyzan.net>
2021-03-05 02:42:40 +0000

    De-lint

--- update_blinkt.py
+++ update_blinkt.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+"""Read Octopus Agile price data from an existing SQLite database and update a
+   Pimoroni Blinkt! display."""
+
 import sqlite3
 from urllib.request import pathname2url
 import blinkt
 
 # set brightness, IT BURNS MEEEEEE, 100 is the max
-brightness = 10
+BRIGHTNESS = 10
 
 # define colour values for LEDs. Edit as you please...
 colourmap = { 'magenta': { 'r': 155, 'g': 0, 'b': 200 },
@@ -17,18 +20,25 @@ colourmap = { 'magenta': { 'r': 155, 'g': 0, 'b': 200 },
               'blue': { 'r': 0, 'g': 0, 'b': 255 }, }
 
 def price_to_colour(price):
-# edit this function to change price thresholds - be careful that you 
-# don't leave gaps in the numbers or strange things will very likely happen.
-# prices are including VAT in p/kWh
+    """edit this function to change price thresholds - be careful that you
+    don't leave gaps in the numbers or strange things will very likely happen.
+    prices are including VAT in p/kWh"""
 
-    if price > 28: return 'magenta'
-    elif 28 >= price > 17: return 'red'
-    elif 17 >= price > 13.5: return 'orange'
-    elif 13.5 >= price > 10: return 'yellow'
-    elif 10 >= price > 5: return 'green'
-    elif 5 >= price > 0: return 'cyan'
-    elif price <= 0: return 'blue'
-    else: raise SystemExit("Can't continue - price of " + str(price) +" doesn't make sense.")
+    if price > 28:
+        return 'magenta'
+    if 28 >= price > 17:
+        return 'red'
+    if 17 >= price > 13.5:
+        return 'orange'
+    if 13.5 >= price > 10:
+        return 'yellow'
+    if 10 >= price > 5:
+        return 'green'
+    if 5 >= price > 0:
+        return 'cyan'
+    if price <= 0:
+        return 'blue'
+    raise SystemExit("Can't continue - price of " + str(price) +" doesn't make sense.")
 
 try:
     # connect to the database in rw mode so we can catch the error if it doesn't exist
@@ -36,19 +46,20 @@ try:
     conn = sqlite3.connect(dburi, uri=True)
     cursor = conn.cursor()
     print('Connected to database...')
-except sqlite3.OperationalError:
+except sqlite3.OperationalError as error:
     # handle missing database case
-    raise SystemExit('Database not found - you need to run store_prices.py first.')
+    raise SystemExit('Database not found - you need to run store-prices.py first.') from error
 
 cursor.execute("SELECT * FROM prices WHERE valid_from > datetime('now', '-30 minutes') LIMIT 8")
 rows = cursor.fetchall()
- 
+
 # finish up the database operation
-if (conn):
+if conn:
     conn.commit()
     conn.close()
 
-if not (len(rows) >= 8): print('Not enough data to fill the display - we will get dark pixels.')
+if not len(rows) >= 8:
+    print('Not enough data to fill the display - we will get dark pixels.')
 
 blinkt.clear()
 
@@ -59,7 +70,7 @@ for row in rows:
     r = colourmap[price_to_colour(row[1])]['r']
     g = colourmap[price_to_colour(row[1])]['g']
     b = colourmap[price_to_colour(row[1])]['b']
-    blinkt.set_pixel(i, r, g, b, brightness/100)
+    blinkt.set_pixel(i, r, g, b, BRIGHTNESS/100)
     i += 1
 
 print ("Setting display...")
