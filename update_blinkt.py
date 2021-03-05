33ad9fb7194f4cf15a08c15ef6cfad34ff4f0c47
James Herbert <james@artyzan.net>
2021-03-05 11:27:37 +0000

    Implement demo mode

--- update_blinkt.py
+++ update_blinkt.py
@@ -5,6 +5,7 @@
 
 import sqlite3
 from urllib.request import pathname2url
+import argparse
 import blinkt
 
 # set brightness, IT BURNS MEEEEEE, 100 is the max
@@ -39,41 +40,59 @@ def price_to_colour(price):
     if price <= 0:
         return 'blue'
     raise SystemExit("Can't continue - price of " + str(price) +" doesn't make sense.")
+    
+parser = argparse.ArgumentParser(description=('Update Blinkt! display using SQLite data'))
+parser.add_argument('--demo', '-d', action='store_true',
+                    help= 'display configured colours, one per pixel',)
+                    
+args = parser.parse_args()
 
-try:
-    # connect to the database in rw mode so we can catch the error if it doesn't exist
-    DB_URI = 'file:{}?mode=rw'.format(pathname2url('agileprices.sqlite'))
-    conn = sqlite3.connect(DB_URI, uri=True)
-    cursor = conn.cursor()
-    print('Connected to database...')
-except sqlite3.OperationalError as error:
-    # handle missing database case
-    raise SystemExit('Database not found - you need to run store_prices.py first.') from error
+if args.demo: 
+    blinkt.clear()
+    i = 0
+    for colour in colourmap:
+        blinkt.set_pixel(i, colourmap[colour]['r'],
+            colourmap[colour]['g'], colourmap[colour]['b'], BRIGHTNESS/100)
+        i += 1
+    print ("Demo mode...")
+    blinkt.set_clear_on_exit(False)
+    blinkt.show()
+    
+else:
+    try:
+        # connect to the database in rw mode so we can catch the error if it doesn't exist
+        DB_URI = 'file:{}?mode=rw'.format(pathname2url('agileprices.sqlite'))
+        conn = sqlite3.connect(DB_URI, uri=True)
+        cursor = conn.cursor()
+        print('Connected to database...')
+    except sqlite3.OperationalError as error:
+        # handle missing database case
+        raise SystemExit('Database not found - you need to run store_prices.py first.') from error
 
-cursor.execute("SELECT * FROM prices WHERE valid_from > datetime('now', '-30 minutes') LIMIT 8")
-rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM prices WHERE valid_from > datetime('now', '-30 minutes') LIMIT 8")
+    rows = cursor.fetchall()
 
-# finish up the database operation
-if conn:
-    conn.commit()
-    conn.close()
+    # finish up the database operation
+    if conn:
+        conn.commit()
+        conn.close()
 
-if not len(rows) >= 8:
-    print('Not enough data to fill the display - we will get dark pixels.')
+    if not len(rows) >= 8:
+        print('Not enough data to fill the display - we will get dark pixels.')
 
-blinkt.clear()
+    blinkt.clear()
 
-i = 0
+    i = 0
 
-for row in rows:
-    print(str(i) + ": " + str(row[1]) + "p - " + str(price_to_colour(row[1])))
-    PIXEL_VALUE_RED = colourmap[price_to_colour(row[1])]['r']
-    PIXEL_VALUE_GREEN = colourmap[price_to_colour(row[1])]['g']
-    PIXEL_VALUE_BLUE = colourmap[price_to_colour(row[1])]['b']
-    blinkt.set_pixel(i, PIXEL_VALUE_RED,
-        PIXEL_VALUE_GREEN, PIXEL_VALUE_BLUE, BRIGHTNESS/100)
-    i += 1
+    for row in rows:
+        print(str(i) + ": " + str(row[1]) + "p - " + str(price_to_colour(row[1])))
+        PIXEL_VALUE_RED = colourmap[price_to_colour(row[1])]['r']
+        PIXEL_VALUE_GREEN = colourmap[price_to_colour(row[1])]['g']
+        PIXEL_VALUE_BLUE = colourmap[price_to_colour(row[1])]['b']
+        blinkt.set_pixel(i, PIXEL_VALUE_RED,
+            PIXEL_VALUE_GREEN, PIXEL_VALUE_BLUE, BRIGHTNESS/100)
+        i += 1
 
-print ("Setting display...")
-blinkt.set_clear_on_exit(False)
-blinkt.show()
+    print ("Setting display...")
+    blinkt.set_clear_on_exit(False)
+    blinkt.show()
