123030294a890a6e6dba0d06fd515b840b1db866
James Herbert <james@artyzan.net>
2021-03-09 20:46:16 +0000

    Fix *really obvious* omission in crontab, thanks mudwalker!

--- install_crontab.sh
+++ install_crontab.sh
@@ -9,9 +9,9 @@ if [ "$(crontab -l | wc -c)" -eq 0 ]; then
       (crontab -l 2>/dev/null; echo "@reboot /bin/sleep 30; cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py --region $1 > ./blinkt.log 2>&1") | crontab -
       (crontab -l 2>/dev/null; echo "@reboot /bin/sleep 40; cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 update_blinkt.py > ./blinkt.log 2>&1") | crontab -
       (crontab -l 2>/dev/null; echo "*/30 * * * * /bin/sleep 5; cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 update_blinkt.py > ./blinkt.log 2>&1") | crontab -
-      (crontab -l 2>/dev/null; echo "30 16 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py > ./blinkt.log 2>&1") | crontab -
-      (crontab -l 2>/dev/null; echo "30 18 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py > ./blinkt.log 2>&1") | crontab -
-      (crontab -l 2>/dev/null; echo "30 20 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py > ./blinkt.log 2>&1") | crontab -;
+      (crontab -l 2>/dev/null; echo "30 16 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py --region $1 > ./blinkt.log 2>&1") | crontab -
+      (crontab -l 2>/dev/null; echo "30 18 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py --region $1 > ./blinkt.log 2>&1") | crontab -
+      (crontab -l 2>/dev/null; echo "30 20 * * * cd /home/pi/agile-blinkt-indicator && /usr/bin/python3 store_prices.py --region $1 > ./blinkt.log 2>&1") | crontab -;
       echo "Done."
     else
       echo "Can't install crontab, $1 is not a valid DNO region".
