5f9aa15d3d090000638d6d7568a5db2cb2e54941
jerbzz <james@artyzan.net>
2021-04-06 21:44:55 +0100

    Use brightness value from config file.

--- update_display.py
+++ update_display.py
@@ -7,11 +7,63 @@ import sqlite3
 from urllib.request import pathname2url
 import argparse
 import blinkt
+import yaml
 
-# set brightness, IT BURNS MEEEEEE, 100 is the max
-BRIGHTNESS = 10
+def deep_get(d, keys, default=None):
+    """
+    Example:
+        d = {'meta': {'status': 'OK', 'status_code': 200}}
+        deep_get(d, ['meta', 'status_code'])          # => 200
+        deep_get(d, ['garbage', 'status_code'])       # => None
+        deep_get(d, ['meta', 'garbage'], default='-') # => '-'
+    """
+    assert type(keys) is list
+    if d is None:
+        return default
+    if not keys:
+        return d
+    return deep_get(d.get(keys[0]), keys[1:], default)
 
-# define colour values for LEDs. Edit as you please...
+def get_config() -> dict:
+    """
+    Read config file and do some basic checks that we have what we need.
+    If not, set sensible defaults or bail out.
+    """
+    try:
+        config_file = open('config.yaml', 'r')
+    except FileNotFoundError:
+        raise SystemExit('Unable to find config.yaml')
+
+    try:
+        config = yaml.safe_load(config_file)
+    except yaml.YAMLError as err:
+        raise SystemExit('Error reading configuration: ' + str(err))
+
+    if config['DisplayType'] == None:
+        raise SystemExit('Error: DisplayType not found in config.yaml')
+
+    if config['DisplayType'] == 'blinkt':
+        print ('Blinkt! display selected.')
+        conf_brightness = deep_get(config, ['Blinkt', 'Brightness'])
+        if not (type(conf_brightness) == int and 5 <= conf_brightness <= 100):
+            print('Misconfigured brightness value: ' + str(conf_brightness) +
+                  '. Using default of 10.')
+            config['Blinkt']['Brightness'] = 10
+
+    elif config['DisplayType'] == 'inkyphat':
+        inky_eeprom = read_eeprom()
+
+        if inky_eeprom == None:
+            raise SystemExit('Error: Inky pHAT display not found')
+        else:
+            print ('Inky pHAT display selected.')
+            
+    else:
+        raise SystemExit('Error: unknown DisplayType ' + config['DisplayType'] + ' in config.yaml' )
+    
+    return config
+
+# define colour values for leds. edit as you please...
 COLOUR_MAP = { 'magenta': { 'r': 155, 'g': 0, 'b': 200 },
               'red': { 'r': 255, 'g': 0, 'b': 0 },
               'orange': { 'r': 255, 'g': 30, 'b': 0 },
@@ -23,7 +75,7 @@ COLOUR_MAP = { 'magenta': { 'r': 155, 'g': 0, 'b': 200 },
 def price_to_colour(price: float) -> str:
     """edit this function to change price thresholds - be careful that you
     don't leave gaps in the numbers or strange things will very likely happen.
-    prices are including VAT in p/kWh"""
+    prices are including vat in p/kwh"""
 
     if price > 28:
         pixel_colour = 'magenta'
@@ -47,11 +99,11 @@ def price_to_colour(price: float) -> str:
         pixel_colour = 'blue'
 
     else:
-        raise SystemExit("Can't continue - price of " + str(price) +" doesn't make sense.")
+        raise systemexit("can't continue - price of " + str(price) +" doesn't make sense.")
 
     return pixel_colour
 
-def set_pixel(index: int, this_colour: str):
+def set_pixel(index: int, this_colour: str, this_brightness: int):
     """This function looks up the R, G, and B values for a given colour
     in the 'COLOUR_MAP' dictionary and passes them to the blinkt! set_pixel method."""
 
@@ -59,7 +111,7 @@ def set_pixel(index: int, this_colour: str):
     pixel_value_green = COLOUR_MAP[this_colour]['g']
     pixel_value_blue = COLOUR_MAP[this_colour]['b']
     blinkt.set_pixel(index, pixel_value_red,
-                     pixel_value_green, pixel_value_blue, BRIGHTNESS/100)
+                     pixel_value_green, pixel_value_blue, this_brightness/100)
 
 parser = argparse.ArgumentParser(description=('Update Blinkt! display using SQLite data'))
 parser.add_argument('--demo', '-d', action='store_true',
@@ -71,7 +123,7 @@ if args.demo:
     blinkt.clear()
     i = 0
     for colour in COLOUR_MAP:
-        set_pixel(i, colour)
+        set_pixel(i, colour, 10)
         i += 1
     print ("Demo mode...")
     blinkt.set_clear_on_exit(False)
@@ -96,8 +148,10 @@ else:
         conn.commit()
         conn.close()
 
-    if not len(price_data_rows) >= 8:
+    if len(price_data_rows) < 8:
         print('Not enough data to fill the display - we will get dark pixels.')
+
+    config = get_config()
 
     blinkt.clear()
 
@@ -106,7 +160,7 @@ else:
         slot_price = row[1]
         this_pixel_colour = price_to_colour(slot_price) # pylint: disable=I0011,C0103
         print(str(i) + ": " + str(slot_price) + "p = " + this_pixel_colour)
-        set_pixel(i, this_pixel_colour)
+        set_pixel(i, this_pixel_colour, config['Blinkt']['Brightness'])
         i += 1
 
     print ("Setting display...")
