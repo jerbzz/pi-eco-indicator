6587e1ab3bcb91e59cbdd3b781468d9c6244ca20
sidewinder42 <d.e.browne@gmail.com>
2023-06-18 20:10:50 +0100

    Update eco_indicator.py

--- eco_indicator.py
+++ eco_indicator.py
@@ -435,6 +435,10 @@ def update_inky(conf: dict, inky_data: dict, demo: bool):
             draw.line((x_pos, average_line_ypos, x_pos + 2, average_line_ypos),
                       inky_display.BLACK)
 
+#Flip orientation if option is set
+    if conf['DisplayOrientation'] == 'inverted':
+        img=img.rotate(180)
+    
     inky_display.set_image(img)
     inky_display.show()
 
@@ -552,6 +556,13 @@ def get_config(filename: str) -> dict:
     else:
         raise SystemExit('Error: unknown DisplayType ' + _config['DisplayType'] + ' in ' + filename)
 
+    if _config['DisplayOrientation'] == 'standard':
+        print('Standard display orientation.')
+    elif _config['DisplayOrientation'] == 'inverted':
+        print('Inverted display orientation.')
+    else:
+        raise SystemExit('Error: Unknown display orientation found in ' + filename + ': ' + _config['DisplayOrientation'])
+    
     if _config['Mode'] is None:
         raise SystemExit('Error: Mode not found in ' + filename)
 
